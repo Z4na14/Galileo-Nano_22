aecdfbd4f304ea91f23b3794e9552d8f7502b981
elZanaorio <jorgesaghin@gmail.com>
2022-04-07 08:40:07 +0200

    Si

--- src/Linux/main.py
+++ src/Linux/main.py
@@ -8,12 +8,13 @@ try:
 except ImportError:
     from smbus import SMBus
 
-print("""temperature-and-pressure.py - Displays the temperature and pressure.
 
-Press Ctrl+C to exit!
-
-""")
 
 # Initialise the BMP280
-bus = SMBus(1)
-bmp280 = BMP280(i2c_dev=bus)
+bmp280 = BMP280(i2c_dev="**Aqui va la dirección I2C**")
+
+while True:
+    temperature = bmp280.get_temperature()
+    pressure = bmp280.get_pressure()
+    print('{:05.2f}*C {:05.2f}hPa'.format(temperature, pressure))
+    time.sleep(1)
