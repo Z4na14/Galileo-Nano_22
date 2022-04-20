a9be82142d02928aea2ad02841f5546064ddd13f
elZanaorio <jorgesaghin@gmail.com>
2022-04-20 16:28:37 +0200

    4 tablas
    Aun está en desarrollo pero tengo la estructura ya está montada

--- src/PARTE FUNCIONAL/Windows/tabla.py
+++ src/PARTE FUNCIONAL/Windows/tabla.py
@@ -1,40 +1,117 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import random
+from random import randint
 
 # Creamos la figura
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = []
+ax = fig.add_subplot(2, 2, 1)
+bx = fig.add_subplot(2, 2, 2)
+cx = fig.add_subplot(2, 2, 3)
+dx = fig.add_subplot(2, 2, 4)
+
+
+class Argumentos:
+    xs = []
+    ys = []
+
+    def __init__(self, xs, ys):
+        self.xs = xs
+        self.ys = ys
+
+
+tab1 = Argumentos
+tab2 = Argumentos
+tab3 = Argumentos
+tab4 = Argumentos
 
 
 # Función utilizada por al función de animar
 
-def animate(i, xs, ys):
-    # Temperatura random
-    temp_c = random.randint(20, 50)
+def Temperatura(i, xs, ys):
+
+    plt.subplot(2, 2, 1)
+
+    # Datos random
+    temp_c = randint(20, 50)
 
     # Añandiendo los datos
     tiempo = dt.datetime.now().strftime("%H:%M:%S")
     xs.append(tiempo)
     ys.append(temp_c)
 
-    print(f"{tiempo}, {temp_c}")
-
     # Redibujar la grafica
     ax.clear()
-    ax.plot(ys[-400:],)
+    ax.plot(ys[-400:], )
 
     # Cambiar el formato de la tabla
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
     plt.title('TEMPERATURA')
-    plt.ylabel('Temperatura (grados)')
+
+
+def Presion(i, xs, ys):
+
+    plt.subplot(2, 2, 2)
+
+    # Datos random
+    temp_c = randint(20, 50)
+
+    # Añandiendo los datos
+    tiempo = dt.datetime.now().strftime("%H:%M:%S")
+    xs.append(tiempo)
+    ys.append(temp_c)
+
+    # Redibujar la grafica
+    bx.clear()
+    bx.plot(ys[-400:], )
+
+    # Cambiar el formato de la tabla
+    plt.title('PRESIÓN')
+
+
+def Radiacion(i, xs, ys):
+
+    plt.subplot(2, 2, 3)
+
+    # Datos random
+    temp_c = randint(20, 50)
+
+    # Añandiendo los datos
+    tiempo = dt.datetime.now().strftime("%H:%M:%S")
+    xs.append(tiempo)
+    ys.append(temp_c)
+
+    # Redibujar la grafica
+    cx.clear()
+    cx.plot(ys[-400:], )
+
+    # Cambiar el formato de la tabla
+    plt.title('RADIACIÓN')
+
+
+def Luz(i, xs, ys):
+
+    plt.subplot(2, 2, 4)
+
+    # Datos random
+    temp_c = randint(20, 50)
+
+    # Añandiendo los datos
+    tiempo = dt.datetime.now().strftime("%H:%M:%S")
+    xs.append(tiempo)
+    ys.append(temp_c)
+
+    # Redibujar la grafica
+    dx.clear()
+    dx.plot(ys[-400:], )
+
+    # Cambiar el formato de la tabla
+    plt.title('LUZ')
 
 
 if __name__ == "__main__":
-    # Función animate
-    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+
+    ani = animation.FuncAnimation(fig, Temperatura, fargs=(tab1.xs, tab1.ys), interval=1000)
+    ani = animation.FuncAnimation(fig, Presion, fargs=(tab2.xs, tab2.ys), interval=1000)
+    ani = animation.FuncAnimation(fig, Radiacion, fargs=(tab3.xs, tab3.ys), interval=100)
+    ani = animation.FuncAnimation(fig, Luz, fargs=(tab4.xs, tab4.ys), interval=1000)
     plt.show()
