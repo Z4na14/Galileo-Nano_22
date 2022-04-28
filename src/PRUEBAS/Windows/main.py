1552287c998e28a858b790e9d72765a734210047
elZanaorio <jorgesaghin@gmail.com>
2022-04-28 17:12:30 +0200

    Exportación de datos
    Semicompletado. Solo falta la creación de tablas para ver la información completa en excel.

--- src/PRUEBAS/Windows/main.py
+++ src/PRUEBAS/Windows/main.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.widgets import Button
 from matplotlib import rcParams
-from random import randint
-
+from numpy.random import randint as np
 from openpyxl import Workbook
 import PIL
 
@@ -34,32 +33,6 @@ plt.subplots_adjust(top=0.934,
                     wspace=0.092)
 
 
-def empezar(val):
-    print("Adios")
-
-
-def exportar(val):
-
-    cont = 0
-
-    for dato in ws1.iter_cols(min_row=2, max_row=len(tab1.xs + 1), max_col=2):
-        if cont <= len(tab1.xs):
-            dato = tab1.ys[cont]
-            cont += 1
-        if cont > len(tab1.xs):
-            dato = tab1.xs[cont - len(tab1.xs)]
-            cont += 1
-
-
-axes1 = plt.axes([0.64, 0.008, 0.15, 0.065])
-bempezar = Button(axes1, 'Empezar recogida')
-bempezar.on_clicked(empezar)
-
-axes2 = plt.axes([0.82, 0.008, 0.15, 0.065])
-bacabar = Button(axes2, 'Exportar XLSX')
-bacabar.on_clicked(exportar)
-
-
 class Argumentos:
     xs = []
     ys = []
@@ -80,12 +53,23 @@ wb = Workbook()
 ws1 = wb.active
 ws1.title = "Temperatura"
 ws1.sheet_properties.tabColor = 'f07a7a'
+ws1.column_dimensions['A'].width = 18
+ws1.column_dimensions['B'].width = 18
+
 ws2 = wb.create_sheet("Presión")
 ws2.sheet_properties.tabColor = '87c8e0'
+ws2.column_dimensions['A'].width = 18
+ws2.column_dimensions['B'].width = 18
+
 ws3 = wb.create_sheet("Radiación")
 ws3.sheet_properties.tabColor = '8ce690'
+ws3.column_dimensions['A'].width = 18
+ws3.column_dimensions['B'].width = 18
+
 ws4 = wb.create_sheet("Luz")
 ws4.sheet_properties.tabColor = 'eff0a8'
+ws4.column_dimensions['A'].width = 18
+ws4.column_dimensions['B'].width = 18
 
 ws1['B1'] = 'TIEMPO'
 ws1['A1'] = 'LECTURAS'
@@ -100,7 +84,7 @@ ws4['B1'] = 'TIEMPO'
 ws4['A1'] = 'LECTURAS'
 
 
-# Función utilizada por al función de animar
+# Función utilizada por al función de animate
 
 def Animation(i, a, b, c, d):
     hora = dt.now().strftime("%H:%M:%S")
@@ -109,12 +93,13 @@ def Animation(i, a, b, c, d):
 
     plt.subplot(2, 2, 1)
     # Datos random
-    temp_a = randint(20, 50)
+    temp_a = np(20, 50)
     # Añandiendo los datos
     a.xs.append(hora)
     a.ys.append(temp_a)
     # Redibujar la grafica
     ax.clear()
+    # if not paused:
     ax.plot(a.ys[-400:], )
     # Cambiar el formato de la tabla
     plt.title('TEMPERATURA')
@@ -123,12 +108,13 @@ def Animation(i, a, b, c, d):
 
     plt.subplot(2, 2, 2)
     # Datos random
-    temp_b = randint(20, 50)
+    temp_b = np(20, 50)
     # Añandiendo los datos
     b.xs.append(hora)
     b.ys.append(temp_b)
     # Redibujar la grafica
     bx.clear()
+    # if not paused:
     bx.plot(b.ys[-400:], )
     # Cambiar el formato de la tabla
     plt.title('PRESIÓN')
@@ -137,12 +123,13 @@ def Animation(i, a, b, c, d):
 
     plt.subplot(2, 2, 3)
     # Datos random
-    temp_c = randint(20, 50)
+    temp_c = np(20, 50)
     # Añandiendo los datos
     c.xs.append(hora)
     c.ys.append(temp_c)
     # Redibujar la grafica
     cx.clear()
+    # if not paused:
     cx.plot(c.ys[-400:], )
     # Cambiar el formato de la tabla
     plt.title('RADIACIÓN')
@@ -151,18 +138,56 @@ def Animation(i, a, b, c, d):
 
     plt.subplot(2, 2, 4)
     # Datos random
-    temp_d = randint(20, 50)
+    temp_d = np(20, 50)
     # Añandiendo los datos
     d.xs.append(hora)
     d.ys.append(temp_d)
     # Redibujar la grafica
     dx.clear()
+    # if not paused:
     dx.plot(d.ys[-400:], )
     # Cambiar el formato de la tabla
     plt.title('LUZ')
 
 
-if __name__ == "__main__":
-    ani = animation.FuncAnimation(fig, Animation, fargs=(tab1, tab2, tab3, tab4), interval=1000)
-    plt.show()
-    register(wb.save, 'Datos.xlsx')
+"""
+paused = True
+
+
+def empezar(val):
+    paused = False
+"""
+
+
+def exportar(val):
+    nom = None
+    for a in range(2):
+        for cell in range((len(tab1.xs))):
+            if a == 0:
+                nom = 'A' + str(cell + 2)
+                ws1[nom] = tab1.ys[cell]
+                ws2[nom] = tab2.ys[cell]
+                ws3[nom] = tab3.ys[cell]
+                ws4[nom] = tab4.ys[cell]
+            if a == 1:
+                nom = 'B' + str(cell + 2)
+                ws1[nom] = tab1.xs[cell]
+                ws2[nom] = tab2.xs[cell]
+                ws3[nom] = tab3.xs[cell]
+                ws4[nom] = tab4.xs[cell]
+
+
+axes1 = plt.axes([0.64, 0.008, 0.15, 0.065])
+bempezar = Button(axes1, 'Empezar recogida')
+# bempezar.on_clicked(empezar)
+
+axes2 = plt.axes([0.82, 0.008, 0.15, 0.065])
+bacabar = Button(axes2, 'Exportar XLSX')
+bacabar.on_clicked(exportar)
+
+ani = animation.FuncAnimation(fig=fig,
+                              func=Animation,
+                              fargs=(tab1, tab2, tab3, tab4),
+                              interval=1000)
+plt.show()
+register(wb.save, 'Datos.xlsx')
