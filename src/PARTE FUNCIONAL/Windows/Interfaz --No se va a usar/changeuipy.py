96012a9e5c1e7d3f7a8bdc0a56e80d6cec807bab
elZanaorio <jorgesaghin@gmail.com>
2022-05-03 18:42:50 +0200

    Borrar cosas innecesarias

--- src/PARTE FUNCIONAL/Windows/Interfaz --No se va a usar/changeuipy.py
+++
@@ -1,4 +0,0 @@
-import os
-archivo = str(input("Como se llama el archivo: "))
-longitud = len(archivo)
-os.system(f"C:/Users/jorge/Desktop/Repos/Galieo-Nano_22/Scripts/pyuic5.exe {archivo} -o {archivo[:longitud-3]+'.py'}")
